ee5b90172b83124d9bfa71f77f2231d57ab2cc55
Guilherme Castello <guilherme.castello.clear@gmail.com>
2023-12-09 10:13:51 -0300

    Feat: Listing ingredients

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,7 @@ from django.urls import (
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 app_name = 'recipe'
 
 urlpatterns = [
