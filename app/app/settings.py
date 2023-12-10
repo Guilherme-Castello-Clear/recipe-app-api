a6be836240379e415316656161d7269865d0effa
Guilherme Castello <guilherme.castello.clear@gmail.com>
2023-12-10 14:45:58 -0300

    Feat: Added Tag and Ingredient filter and test for it

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6jq4xm53+f01v%#cpnb_!)%b9o+dfi0@uh%fe2ma+84&sm@+!t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
